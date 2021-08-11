171fbba84c45295f152f27087f4c04b833b31dc5
James-Yoel <james.yoel15@gmail.com>
2021-08-11 16:20:10 +0700

    test message

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>248f1748-69db-4d26-96f7-97132cf1f763</testSuiteGuid>
+   <testCaseLink>
+      <guid>58e29c1d-7ffe-4121-992c-b5c6c1d12f72</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Tom Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc857d70-e999-4708-99c5-1a1f21d14378</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kim Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
